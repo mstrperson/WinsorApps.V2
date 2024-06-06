aafe29f33de1cfaddcfde0a3b25f7a9403781392
Jason Cox <coxj@vt.edu>
2024-06-06 08:09:49 -0400

    Gadget Service!

---
+++ WinsorApps.Services.Global/Models/RecurringEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinsorApps.Services.Global.Models;
+
+public readonly record struct CycleDayRecurringEvent(string id, DateOnly beginning, DateOnly ending, string creatorId,
+        string summary, string description, ImmutableArray<string> attendees, bool allDay, TimeOnly time, int duration,
+        ImmutableArray<string> cycleDays, int frequency, bool isPublic);
+
+public readonly record struct CreateRecurringEvent(DateOnly beginning, DateOnly ending, string summary, string description,
+    ImmutableArray<string> attendees, ImmutableArray<string> cycleDays, int frequency,
+    bool isPublic = false, bool allDay = false, TimeOnly time = default, int duration = 0);
---
+++ WinsorApps.Services.Global/Services/GadgetService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinsorApps.Services.Global.Models;
+
+namespace WinsorApps.Services.Global.Services
+{
+    public partial class GadgetService : IAsyncInitService, ICacheService
+    {
+        private readonly ApiService _api;
+
+        public event EventHandler? OnCacheRefreshed;
+
+        public ImmutableArray<CycleDayRecurringEvent> RecurringEvents { get; private set; } = [];
+
+        public GadgetService(ApiService api)
+        {
+            _api = api;
+        }
+
+        public bool Started { get; private set; }
+
+        public bool Ready { get; private set; }
+
+        public double Progress { get; private set; }
+
+        public async Task Initialize(ErrorAction onError)
+        {
+            while (!_api.Ready)
+                await Task.Delay(250);
+            Started = true;
+
+            RecurringEvents = await _api.SendAsync<ImmutableArray<CycleDayRecurringEvent>>(HttpMethod.Get, "api/users/self/cycle-day-recurring-events", onError: onError);
+            Progress = 1;
+
+            Ready = true;
+        }
+
+        public async Task Refresh(ErrorAction onError)
+        {
+            var result = await _api.SendAsync<ImmutableArray<CycleDayRecurringEvent>>(HttpMethod.Get, "api/users/self/cycle-day-recurring-events", onError: onError);
+            if(!result.SequenceEqual(RecurringEvents))
+            {
+                RecurringEvents = result;
+                OnCacheRefreshed?.Invoke(this, EventArgs.Empty);
+            }
+
+        }
+
+        public async Task WaitForInit(ErrorAction onError)
+        {
+            while (!Ready)
+                await Task.Delay(250);
+        }
+
+        public async Task<CycleDayRecurringEvent?> CreateNewEvent(CreateRecurringEvent newEvent, ErrorAction onError)
+        {
+            var result = await _api.SendAsync<CreateRecurringEvent, CycleDayRecurringEvent?>(HttpMethod.Post, "api/users/self/cycle-day-recurring-events", newEvent, onError: onError);
+
+            if(result.HasValue)
+            {
+                RecurringEvents = RecurringEvents.Add(result.Value);
+                OnCacheRefreshed?.Invoke(this, EventArgs.Empty);
+            }
+
+            return result;
+        }
+
+        public async Task<CycleDayRecurringEvent> UpdateEvent(string eventId, CreateRecurringEvent newEvent, ErrorAction onError)
+        {
+            var result = await _api.SendAsync<CreateRecurringEvent, CycleDayRecurringEvent?>(HttpMethod.Put, $"api/users/self/cycle-day-recurring-events/{eventId}", newEvent, onError: onError);
+
+            var oldEvent = RecurringEvents.FirstOrDefault(evt => evt.id == eventId);
+
+            if (result.HasValue)
+            {
+                if (!string.IsNullOrEmpty(oldEvent.id))
+                    RecurringEvents = RecurringEvents.Remove(oldEvent);
+                RecurringEvents = RecurringEvents.Add(result.Value);
+                OnCacheRefreshed?.Invoke(this, EventArgs.Empty);
+            }
+
+            return result ?? oldEvent;
+        }
+    }
+}
