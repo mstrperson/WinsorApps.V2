74f71c03b270d65fe051e71d2542020b73e702e7
Jason Cox <tszhuen.zuo@winsor.edu>
2024-06-20 12:08:47 -0700

    yes

--- WinsorApps.MAUI.CDRE/ViewModels/RecurringEventViewModel.cs
+++ WinsorApps.MAUI.CDRE/ViewModels/RecurringEventViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Controls.PlatformConfiguration.TizenSpecific;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,11 +18,26 @@ namespace WinsorApps.MAUI.CDRE.ViewModels
     public partial class RecurringEventViewModel :
         ObservableObject,
         ICachedViewModel<RecurringEventViewModel, CycleDayRecurringEvent, CycleDayRecurringEventService>,
-        ISelectable<RecurringEventViewModel>
+        ISelectable<RecurringEventViewModel>,
+        IDefaultValueViewModel<RecurringEventViewModel>,
+        IErrorHandling
     {
         private readonly CycleDayRecurringEventService _eventService = ServiceHelper.GetService<CycleDayRecurringEventService>();
 
         [ObservableProperty] string id = "";
+        [ObservableProperty] DateOnly beginning;
+        [ObservableProperty] DateOnly ending;
+        [ObservableProperty] string creatorId = "";
+        [ObservableProperty] string summary = "";
+        [ObservableProperty] string description = "";
+        [ObservableProperty] ImmutableArray<string> attendees;
+        [ObservableProperty] bool allDay;
+        [ObservableProperty] TimeOnly startTime;
+        [ObservableProperty] TimeOnly endTime;
+        [ObservableProperty] ImmutableArray<string> cycleDays;
+        [ObservableProperty] int frequency;
+        [ObservableProperty] bool isPublic;
+        public int Duration => (int)(EndTime - StartTime).TotalMinutes;
 
         // TODO:  Add More Observable Properties for all the relevant
         //        things for a CycleDayRecurringEvent
@@ -30,6 +47,23 @@ namespace WinsorApps.MAUI.CDRE.ViewModels
             
         }
 
+        [RelayCommand]
+        public async Task Submit()
+        {
+            CreateRecurringEvent create = new CreateRecurringEvent
+                (Beginning, Ending, Summary, Description, Attendees, CycleDays, Frequency, IsPublic, AllDay, StartTime, Duration);
+            if (string.IsNullOrEmpty(Id))
+            {
+                var result = await _eventService.CreateNewEvent(create, OnError.DefaultBehavior(this));
+                if (result.HasValue)
+                {
+                    Id = result.Value.id;
+                }
+                return;
+            }
+
+            await _eventService.UpdateEvent(Id, create, OnError.DefaultBehavior(this));
+        }
 
         #region ISelectable stuff
         [ObservableProperty] bool isSelected;
@@ -38,6 +72,7 @@ namespace WinsorApps.MAUI.CDRE.ViewModels
         /// Do something if this Recurring Event is Selected.
         /// </summary>
         public event EventHandler<RecurringEventViewModel>? Selected;
+        public event EventHandler<ErrorRecord>? OnError;
 
         [RelayCommand]
         public void Select()
@@ -52,6 +87,8 @@ namespace WinsorApps.MAUI.CDRE.ViewModels
         #region ICachedViewModel stuff
         public static ConcurrentBag<RecurringEventViewModel> ViewModelCache { get; private set; } = [];
 
+        public static RecurringEventViewModel Default => new();
+
         public static RecurringEventViewModel Get(CycleDayRecurringEvent model)
         {
             var vm = ViewModelCache.First(re => re.Id == model.id);
@@ -61,6 +98,20 @@ namespace WinsorApps.MAUI.CDRE.ViewModels
             vm = new()
             {
                 Id = model.id,
+                Beginning=model.beginning,
+                Ending=model.ending,
+                CreatorId=model.creatorId,
+                Summary=model.summary,
+                Description=model.description,
+                Attendees=model.attendees,
+                AllDay=model.allDay,
+                StartTime=model.time,
+                EndTime=model.time.AddMinutes(model.duration),
+                CycleDays=model.cycleDays,
+                Frequency=model.frequency,
+                IsPublic=model.isPublic,
+
+
                 // TODO: Initialize the rest of the ObservableProperties you add.
             };
             ViewModelCache.Add(vm);
