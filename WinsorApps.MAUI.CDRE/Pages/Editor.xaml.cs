8ed7c8942ad1d0f6c8f5b6712469ae531093d428
Susan Zuo <tszhuen.zuo@winsor.edu>
2024-07-02 06:55:31 -0700

    heyyyy

--- WinsorApps.MAUI.CDRE/Pages/Editor.xaml.cs
+++ WinsorApps.MAUI.CDRE/Pages/Editor.xaml.cs
@@ -10,5 +10,14 @@ public partial class Editor : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = vm;
+        vm.PropertyChanged += Vm_PropertyChanged;
 	}
+
+    private void Vm_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if(e.PropertyName == "Busy" && ViewModel.Busy)
+		{
+			Loading.Focus();
+		}
+    }
 }
