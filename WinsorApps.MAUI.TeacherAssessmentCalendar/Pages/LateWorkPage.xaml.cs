01f1a28921fe12b6b921e61b416a77931a6c525f
Jason Cox <coxj@vt.edu>
2024-09-10 14:08:30 -0400

    fucking macos i swear to go

--- WinsorApps.MAUI.TeacherAssessmentCalendar/Pages/LateWorkPage.xaml.cs
+++ WinsorApps.MAUI.TeacherAssessmentCalendar/Pages/LateWorkPage.xaml.cs
@@ -6,6 +6,8 @@ namespace WinsorApps.MAUI.TeacherAssessmentCalendar.Pages;
 
 public partial class LateWorkPage : ContentPage
 {
+	public LateWorkPageViewModel ViewModel => (LateWorkPageViewModel)BindingContext;
+
 	public LateWorkPage(LateWorkPageViewModel vm)
 	{
 		vm.OnError += this.DefaultOnErrorHandler();
@@ -13,4 +15,13 @@ public partial class LateWorkPage : ContentPage
 		vm.Initialize(this.DefaultOnErrorAction()).SafeFireAndForget(e => e.LogException());
 		InitializeComponent();
 	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		if (!ViewModel.ShowSelectedSection)
+			return;
+
+		var page = new SectionLateWorkViewPage() { BindingContext = ViewModel.SelectedSection };
+		Navigation.PushAsync(page);
+    }
 }
