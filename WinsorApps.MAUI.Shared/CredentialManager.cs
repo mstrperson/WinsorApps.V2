c54708d38c64d55e9fc8c061f4f30aac59543944
Jason Cox <coxj@vt.edu>
2024-12-30 14:31:44 -0500

    Fitness Center Admin works

--- WinsorApps.MAUI.Shared/CredentialManager.cs
+++ WinsorApps.MAUI.Shared/CredentialManager.cs
@@ -8,64 +8,69 @@ using AsyncAwaitBestPractices;
 using WinsorApps.Services.Global;
 using WinsorApps.Services.Global.Models;
 
-namespace WinsorApps.MAUI.Shared
+namespace WinsorApps.MAUI.Shared;
+
+public class CredentialManager : ISavedCredential
 {
-    public class CredentialManager : ISavedCredential
+    private static readonly string credKey = "edu.winsor.forms_credential";
+
+    public async Task CheckSavedCredentials()
     {
-        private static readonly string credKey = "edu.winsor.forms_credential";
+        var thisCred = await GetSavedCredential();
+        if (thisCred.HasValue) return;
 
-        public async Task CheckSavedCredentials()
+        SavedCredential sc = new();
+        if (!sc.SavedCredExists) return;
+
+        var result = await sc.GetSavedCredential();
+        if (!result.HasValue) return;
+
+        var cred = result.Value;
+        await Save(cred.SavedEmail, cred.SavedPassword, cred.JWT, cred.RefreshToken);
+    }
+
+    public bool SavedCredExists { get; private set; } = false;
+
+    public async Task DeleteSavedCredential()
+    {
+        SecureStorage.Remove(credKey);
+        SavedCredExists = false;
+        await Task.CompletedTask;
+    }
+
+    public async Task<Credential?> GetSavedCredential()
+    {
+        SavedCredExists = false;
+        var json = await SecureStorage.GetAsync(credKey);
+        if (string.IsNullOrEmpty(json)) return null;
+
+        try
         {
-            var thisCred = await GetSavedCredential();
-            if (thisCred.HasValue) return;
-            
-            SavedCredential sc = new();
-            if (!sc.SavedCredExists) return;
-            
-            var result = await sc.GetSavedCredential();
-            if (!result.HasValue) return;
-            
-            var cred = result.Value;
-            await Save(cred.SavedEmail, cred.SavedPassword, cred.JWT, cred.RefreshToken);
+            var cred = JsonSerializer.Deserialize<Credential>(json);
+            SavedCredExists = true;
+            return cred;
         }
-        
-        public bool SavedCredExists { get; private set; } = false;
-
-        public async Task DeleteSavedCredential()
+        catch
         {
-            SecureStorage.Remove(credKey);
-            await Task.CompletedTask;
-        }
-
-        public async Task<Credential?> GetSavedCredential()
-        {
-            var json = await SecureStorage.GetAsync(credKey);
-            if(string.IsNullOrEmpty(json)) return null;
-
-            try
-            {
-                var cred = JsonSerializer.Deserialize<Credential>(json);
-                return cred;
-            }
-            catch
-            {
-                return null;    
-            }
-        }
-
-        public async Task Save(string email, string password, string jwt = "", string refreshToken = "")
-        {
-            Credential credential = new(email, password, jwt, refreshToken);
-            var json = JsonSerializer.Serialize(credential);
-            await SecureStorage.SetAsync(credKey, json);
-        }
-
-        public async Task SaveJwt(string jwt, string refreshToken)
-        {
-            var credential = await GetSavedCredential() ?? new();
-            credential = credential with { JWT = jwt, RefreshToken = refreshToken };
-            var json = JsonSerializer.Serialize(credential);
-            await SecureStorage.SetAsync(credKey, json);
+            return null;
         }
     }
+
+    public async Task Save(string email, string password, string jwt = "", string refreshToken = "")
+    {
+        Credential credential = new(email, password, jwt, refreshToken);
+        var json = JsonSerializer.Serialize(credential);
+        await SecureStorage.SetAsync(credKey, json);
+        SavedCredExists = true;
+    }
+
+    public async Task SaveJwt(string jwt, string refreshToken)
+    {
+        var credential = await GetSavedCredential() ?? new();
+        credential = credential with {JWT = jwt, RefreshToken = refreshToken};
+        var json = JsonSerializer.Serialize(credential);
+        await SecureStorage.SetAsync(credKey, json);
+        SavedCredExists = true;
+    }
 }
+
