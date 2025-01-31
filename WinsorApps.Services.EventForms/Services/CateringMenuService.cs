7dce685569e6459d7040fb30027159c01c7c66c5
Jason Cox <coxj@vt.edu>
2025-01-31 07:50:18 -0500

    Reapply "cache renewal every two weeks."
    This reverts commit 48b14361824e53f0a476be42789b8fc8e5b7ef6d.

--- WinsorApps.Services.EventForms/Services/CateringMenuService.cs
+++ WinsorApps.Services.EventForms/Services/CateringMenuService.cs
@@ -77,6 +77,18 @@ namespace WinsorApps.Services.EventForms.Services
             if (!File.Exists($"{_logging.AppStoragePath}{CacheFileName}"))
                 return false;
 
+
+            var cacheAge = DateTime.Now - File.GetCreationTime($"{_logging.AppStoragePath}{CacheFileName}");
+
+            _logging.LogMessage(LocalLoggingService.LogLevel.Information,
+                $"{CacheFileName} is {cacheAge.TotalDays:0.0} days old.");
+
+            if (cacheAge.TotalDays > 14)
+            {
+                _logging.LogMessage(LocalLoggingService.LogLevel.Information, "Deleting Aged Cache File.");
+                File.Delete($"{_logging.AppStoragePath}{CacheFileName}");
+                return false;
+            }
             try
             {
                 var json = File.ReadAllText($"{_logging.AppStoragePath}{CacheFileName}");
